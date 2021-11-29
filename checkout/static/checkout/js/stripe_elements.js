f0adf56aca53a6eb8bc329fd5d4493c8d15721a2
KSheeds1 <katiesheedy@hotmail.com>
2021-11-29 18:37:48 +0000

    Added basic checkout functionality.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,14 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+// Get Stripe public key and client secret from the template
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+
+// Create a variable using public key to set up Stripe:
+var stripe = Stripe(stripePublicKey);
+
+// Use it to create an instance of Stripe Elements:
 var elements = stripe.elements();
+
+// Use style argument to set some basic styles
 var style = {
     base: {
         color: '#000',
@@ -17,5 +24,56 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+// Create card element 
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+//Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit 
+// Customize which fields are collected by the Payment Element
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // Provide user with error result (e.g., insufficient funds)
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // The payment has been processed
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
+
